f50ac48d6dc28e56e78cf4b892b9ae4ac85fae2d
sqs <qslack@qslack.com>
2010-07-19 15:33:38 -0700

    js class and method stubs

--- core/pake.js
+++ core/pake.js
@@ -1,8 +1,53 @@
+"use strict";
+/*jslint indent: 2, bitwise: false, nomen: false, plusplus: false, white: false, regexp: false */
+
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cr = Components.results;
 
 Components.utils.import("resource://gre/modules/ctypes.jsm");
+
+
+var pake = {};
+
+pake.client = function() {
+
+};
+
+pake.client.prototype = {
+    /**
+     * Set auth credentials.
+     * @param {String} username
+     * @param {String} realm
+     * @param {String} password
+     * @return {void}
+     */
+    set_credentials:function (username, realm, password) {
+        this._username = username;
+        this._realm = realm;
+        this._password = password;
+    },
+
+    recv_Y:function (y) {
+
+    },
+
+    compute_h:function () {
+
+    },
+
+    compute_resps:function (tcpcrypt_sid) {
+
+    },
+
+    compute_respc:function (tcpcrypt_sid) {
+
+    },
+
+    debug:function () {
+
+    },
+};
 
 function addjs(a, b) {
     var lib = ctypes.open("/home/sqs/src/jspake/c/add.so"); 
--- test/test_pake.js
+++ test/test_pake.js
@@ -4,4 +4,10 @@ function test_add() {
     print("addjs(2,3) = " + addjs(2,3));
 }
 
+function test_pake_client() {
+    var pc = new pake.client();
+    print("pake.client = " + pc);
+}
+
 test_add();
+test_pake_client();
