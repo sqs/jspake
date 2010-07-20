6bbeef1c2b2492230d26e5458cbd9c4a64ed8ed4
sqs <qslack@qslack.com>
2010-07-20 07:32:20 -0700

    clean up interface

--- core/pake.js
+++ core/pake.js
@@ -28,23 +28,40 @@ pake.client.prototype = {
         this._password = password;
     },
 
-    recv_Y:function (y) {
-
+    /**
+     * @return {void}
+     */
+    set_server_Y:function (server_Y) {
+        
     },
-
-    compute_h:function () {
-
-    },
-
-    compute_resps:function (tcpcrypt_sid) {
-
-    },
-
+    
+    /**
+     * @return {String} respc
+     */
     compute_respc:function (tcpcrypt_sid) {
 
     },
 
+    /**
+     * @param {??} tcpcrypt_sid
+     * @return {bool} True only if server_resps is valid.
+     */
+    check_resps:function (tcpcrypt_sid, server_resps) {
+
+    },
+
+    /**
+     * Prints out a human-readable representation of this object's internal
+     * state.
+     * @return {void}
+     */
     debug:function () {
+
+    },
+
+
+    /*************************************************************************/
+    _compute_h:function () {
 
     },
 };
