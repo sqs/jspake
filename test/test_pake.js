c5dc44c21a2698eba075a3d19416fbe771e599dc
sqs <qslack@qslack.com>
2010-07-20 07:32:11 -0700

    remove old tests, add basic test for pake.client

--- test/test_pake.js
+++ test/test_pake.js
@@ -1,13 +1,8 @@
 load("../core/pake.js");
 
-function test_add() {
-    print("addjs(2,3) = " + addjs(2,3));
-}
-
-function test_pake_client() {
+function test_pake_client_new() {
     var pc = new pake.client();
-    print("pake.client = " + pc);
+    do_check_true(pc);
 }
 
-test_add();
-test_pake_client();
+test_pake_client_new();
