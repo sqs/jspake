8fed38b19c99a2110e14e55d1d7a44a748249690
sqs <qslack@qslack.com>
2010-07-20 07:38:09 -0700

    set_credentials test

--- test/test_pake.js
+++ test/test_pake.js
@@ -5,4 +5,16 @@ function test_pake_client_new() {
     do_check_true(pc);
 }
 
-test_pake_client_new();
+function test_pake_client_set_credentials() {
+    /* kind of a stupid test, but... */
+    var pc = new pake.client();
+    pc.set_credentials("jsmith", "protected area", "jsmith");
+    do_check_eq(pc._username, "jsmith");
+    do_check_eq(pc._realm, "protected area");
+    do_check_eq(pc._password, "jsmith");
+}
+
+function run_test() {
+    test_pake_client_new();
+    test_pake_client_set_credentials();
+}
